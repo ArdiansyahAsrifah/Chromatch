4ad69b075b53f3a61adb2e57aebd1c0ea945fd7c
Muhammad Ardiansyah Asrifah_H071221068 <113808797+ArdiansyahAsrifah@users.noreply.github.com>
2025-06-09 09:12:13 +0700

    Initial Commit

---
+++ Chromatch/ChromatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChromatchApp.swift
+//  Chromatch
+//
+//  Created by Muhammad Ardiansyah Asrifah on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ChromatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChromatchTests/ChromatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChromatchTests.swift
+//  ChromatchTests
+//
+//  Created by Muhammad Ardiansyah Asrifah on 09/06/25.
+//
+
+import Testing
+@testable import Chromatch
+
+struct ChromatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
