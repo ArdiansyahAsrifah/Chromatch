6253440163d1cb6fd62619080697004e26a48adf
Hafi <developer.hafi@gmail.com>
2025-06-17 07:52:30 +0700

    Add Nav Title to tab page
    Add nav title to home, analyze, and history

--- Chromatch/View/Home/NavBar.swift
+++ Chromatch/View/Home/NavBar.swift
@@ -126,18 +126,31 @@ struct NavigationView: View {
     
     var body: some View {
         ZStack(alignment: .bottom) {
-            Group {
-                switch selectedTab {
-                case .home:
-                    HomeView(isActive: $isActive, selectedTab: $selectedTab)
-                        .environmentObject(historyManager)
-                case .scan:
-                    AnalyzeView(selectedTab: $selectedTab)
-                        .environmentObject(historyManager)
-                case .history:
-                    HistoryView()
-                        .environmentObject(historyManager)
+            
+            ZStack(alignment: .top) {
+                // Navigation Bar
+                Group {
+                    switch selectedTab {
+                    case .home:
+                        HomeView(isActive: $isActive, selectedTab: $selectedTab)
+                    case .scan:
+                        AnalyzeView(selectedTab: $selectedTab)
+                    case .history:
+                        HistoryView()
+                    }
                 }
+                .environmentObject(historyManager)
+                
+                // Custom Navigation Title
+                Text(navigationTitle(for: selectedTab))
+                    .font(.caption)
+                    .fontWeight(.medium)
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 10)
+                    .background(.thinMaterial)
+                    .clipShape(Capsule())
+                    .shadow(color: .black.opacity(0.1), radius: 5, y: 5)
+                    .padding(.top, 60)
             }
             
             if selectedTab != .scan {
@@ -145,6 +158,23 @@ struct NavigationView: View {
             }
         }
         .environmentObject(appState)
-        .edgesIgnoringSafeArea(.all)
+        .ignoresSafeArea()
+    }
+    
+    // Helper function to get the correct title for each tab
+    private func navigationTitle(for tab: AppTab) -> String {
+        switch tab {
+        case .home:
+            // Get the latest result to display in the title
+            if let latestResult = historyManager.results.first {
+                return "HOME / \(latestResult.result.uppercased())"
+            } else {
+                return "HOME"
+            }
+        case .scan:
+            return "ANALYZE"
+        case .history:
+            return "HISTORY"
+        }
     }
 }
